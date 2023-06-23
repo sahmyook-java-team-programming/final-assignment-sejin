b708463258a3bfcadfc8f9eb553ab67add78f8bc
sejin0211 <sejin0211@naver.com>
2023-06-23 10:25:59 +0900

    init setting

---
+++ src/main/java/com/ohgiraffers/finalassignmentsejin/FinalAssignmentSejinApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.finalassignmentsejin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAssignmentSejinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalAssignmentSejinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/finalassignmentsejin/FinalAssignmentSejinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.finalassignmentsejin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalAssignmentSejinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
